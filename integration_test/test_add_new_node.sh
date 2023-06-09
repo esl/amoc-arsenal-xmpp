afc911cabf814560b06dfe54a2d216572cd03e9b
Nelson Vides <nelson.vides@erlang-solutions.com>
2023-06-09 12:46:26 +0200

    Rescue lost files from amoc_core upgrade

--- integration_test/test_add_new_node.sh
+++ integration_test/test_add_new_node.sh
@@ -4,7 +4,7 @@ source "$(dirname "$0")/helper.sh"
 enable_strict_mode
 
 docker_compose up -d amoc-worker-3
-# wait_for_healthcheck amoc-worker-3
+wait_for_healthcheck amoc-worker-3
 
 amoc_eval amoc-worker-3 "amoc_controller:get_status()." | contain dummy_scenario running
 amoc_eval amoc-worker-3 "binary_to_list(amoc_config:get(test))." | contain "test_value"
